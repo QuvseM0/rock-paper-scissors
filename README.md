8b26d6e5711a43b3681d046aef51b5b1175e48a8
quvse <marquesoafonso@gmail.com>
2024-09-20 18:37:09 +0100

    started project, first task on hold

---
+++ README.md
@@ -0,0 +1,9 @@
+--quvse--
+
+This is a simple game of Rock Paper Scissors.
+
+lpd === last point dropoff 
+
+lpd = computer choice only returns scissors.
+
+--made following the rock paper scissors project from The Odin Project (TOP)
---
+++ javascript.js
@@ -0,0 +1,18 @@
+/*let rock = "Rock"
+let paper = "Paper"
+let scissors = "Scissors"
+let choice = Math.random()
+
+function getComputerChoice(){
+    
+    if (choice >= 0 && choice <= 0.3)
+        choice = rock
+    else if(choice > 0.3 && choice < 0.6)
+        choice = paper
+    else (choice >= 0.6 && choice < 1)
+        choice = scissors
+
+    return choice
+} */
+
+console.log(getComputerChoice())
