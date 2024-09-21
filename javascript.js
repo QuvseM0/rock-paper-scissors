1c3af5db6301c00251bd5ffef7bbcef0beaafa39
quvse <marquesoafonso@gmail.com>
2024-09-21 23:08:59 +0100

    round logic implemented

--- javascript.js
+++ javascript.js
@@ -1,6 +1,8 @@
 let rock = "rock"
 let paper = "paper"
 let scissors = "scissors"
+let humanScore = 0
+let computerScore = 0
 
 function getComputerChoice(){
     let choice = Math.random()
@@ -14,9 +16,51 @@ function getComputerChoice(){
 } 
 
 
+
 function getHumanChoice(pick){
     pick = prompt("Rock, Paper or Scissors?")
-    return pick = pick.trim().toLowerCase(pick)
+    pick = pick.trim().toLowerCase(pick)
+    return pick
 }
 
-console.log(getHumanChoice())
+function playRound(humanChoice,computerChoice){
+    let win = "you win, " 
+    let lose = "you lose, " 
+    let tie = "you're tied"
+    let state
+    
+    if (humanChoice == computerChoice)
+        return tie
+    else if (humanChoice != computerChoice && humanChoice == rock)
+        if (computerChoice == paper) {
+            ++computerScore
+            state = lose + "killed by paper"
+        } else {
+            ++humanScore
+            state = win + "killed scissors"
+        }
+    else if (humanChoice != computerChoice && humanChoice == paper)
+        if (computerChoice == scissors) {
+            ++computerScore
+            state = lose + "killed by scissors"
+        } else {
+            ++humanScore
+            state = win + "killed rock"
+        }
+    else if (humanChoice != computerChoice && humanChoice == scissors)
+        if (computerChoice == rock) {
+            ++computerScore
+            state = lose + "killed by rock"
+        } else {
+            ++humanScore
+            state = win + "killed paper"
+        }
+    
+    return state
+}
+
+console.log(playRound(getHumanChoice(),getComputerChoice()))
+console.log(humanScore)
+console.log(computerScore)
+
+
