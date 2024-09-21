da10d7677b159ae4883d6b71d6e1dd7f32b4ce86
quvse <marquesoafonso@gmail.com>
2024-09-21 23:36:02 +0100

    I actually did it omg

--- javascript.js
+++ javascript.js
@@ -15,52 +15,53 @@ function getComputerChoice(){
     return choice
 } 
 
-
-
 function getHumanChoice(pick){
     pick = prompt("Rock, Paper or Scissors?")
     pick = pick.trim().toLowerCase(pick)
     return pick
 }
 
-function playRound(humanChoice,computerChoice){
-    let win = "you win, " 
-    let lose = "you lose, " 
-    let tie = "you're tied"
+function playGame(nr){
     let state
-    
-    if (humanChoice == computerChoice)
-        return tie
-    else if (humanChoice != computerChoice && humanChoice == rock)
-        if (computerChoice == paper) {
-            ++computerScore
-            state = lose + "killed by paper"
-        } else {
-            ++humanScore
-            state = win + "killed scissors"
-        }
-    else if (humanChoice != computerChoice && humanChoice == paper)
-        if (computerChoice == scissors) {
-            ++computerScore
-            state = lose + "killed by scissors"
-        } else {
-            ++humanScore
-            state = win + "killed rock"
-        }
-    else if (humanChoice != computerChoice && humanChoice == scissors)
-        if (computerChoice == rock) {
-            ++computerScore
-            state = lose + "killed by rock"
-        } else {
-            ++humanScore
-            state = win + "killed paper"
-        }
-    
-    return state
+    function playRound(humanChoice,computerChoice){
+        let win = "you win, " 
+        let lose = "you lose, " 
+        let tie = "you're tied"
+        if (humanChoice == computerChoice)
+            return tie
+        else if (humanChoice != computerChoice && humanChoice == rock)
+            if (computerChoice == paper) {
+                ++computerScore
+                state = lose + "killed by paper"
+            } else {
+                ++humanScore
+                state = win + "killed scissors"
+            }
+        else if (humanChoice != computerChoice && humanChoice == paper)
+            if (computerChoice == scissors) {
+                ++computerScore
+                state = lose + "killed by scissors"
+            } else {
+                ++humanScore
+                state = win + "killed rock"
+            }
+        else if (humanChoice != computerChoice && humanChoice == scissors)
+            if (computerChoice == rock) {
+                ++computerScore
+                state = lose + "killed by rock"
+            } else {
+                ++humanScore
+                state = win + "killed paper"
+            }
+        return state
+    }
+    for (i = 0; i < nr; i++){
+        console.log(playRound(getHumanChoice(),getComputerChoice()))
+        console.log("User: " + humanScore)
+        console.log("CPU: " + computerScore)   
+    }    
 }
 
-console.log(playRound(getHumanChoice(),getComputerChoice()))
-console.log(humanScore)
-console.log(computerScore)
+playGame(5)
 
 
